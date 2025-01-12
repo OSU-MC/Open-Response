85824fe9880c94c67d4ee0bf39fbbd0f1412d518
enjiwood <87221016+enjiwood@users.noreply.github.com>
2025-01-12 12:02:17 -0800

    Add Redis
    This commit adds a basic redis implementation to aid in future development.

---
+++ server/redis/basics.js
@@ -0,0 +1,32 @@
+const redis = require('redis');
+
+const PORT = 6380;
+const client = redis.createClient({
+    legacyMode: true,
+    PORT: PORT
+});
+
+client.connect().catch(console.error);
+
+client.on('connect', function() {
+    console.log('Redis connection successful');
+    connectionBasics();
+});
+
+async function connectionBasics () {
+    client.set('user:1', 'Student', function(err, reply) {
+        console.log(reply);
+    });
+
+    client.get('user:1', function(err, reply) {
+        console.log(reply);
+    });
+
+    client.exists('user:1', 'Student', function(err, reply) {
+        console.log(reply);
+    });
+
+    client.del('user:1', function(err, reply) {
+        console.log(reply);
+    });
+}
