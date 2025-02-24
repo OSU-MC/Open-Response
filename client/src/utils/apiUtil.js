34ba8b0c61c2702c3ebd2d0a571f78cf08851e1b
enjiwood <87221016+enjiwood@users.noreply.github.com>
2025-02-24 13:48:43 -0800

    Change apiUtil server access point

--- client/src/utils/apiUtil.js
+++ client/src/utils/apiUtil.js
@@ -5,7 +5,7 @@ import Notice from '../components/Notice'
 // TODO: set up config for stuff like CORS
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001',
+    baseURL: import.meta.env.VITE_API_URL || 'http://core.open-response.org',
     withCredentials: true
 });
 
