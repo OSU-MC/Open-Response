e6879bf31c25ce86e1bf7952d74dcc4e6b34e29e
Ethan <ej.laserbread@proton.me>
2025-10-13 16:20:09 -0700

    Fixed a key dependency error that caused Sequelize to crash on MariaDB

--- core/db/migrations/20250118235051-change-grading-attendance.js
+++ core/db/migrations/20250118235051-change-grading-attendance.js
@@ -220,7 +220,6 @@ module.exports = {
         defaultValue: -1, // -1 is used when it is not sorted
         allowNull: false,
       });
-      await queryInterface.removeColumn('QuestionInLectures', 'order');
 
       // Add the new unique constraint on `lectureForSectionId` and `questionId`
       await queryInterface.addConstraint('QuestionInLectures', {
@@ -239,6 +238,9 @@ module.exports = {
         'QuestionInLectures',
         'custom_unique_question_in_lectures_order_constraint'
       );
+      // Delete the old QuestionInLectures.order column. This was moved
+      // down because otherwise MariaDB would have a fit.
+      await queryInterface.removeColumn('QuestionInLectures', 'order');
 
       // Add missing LectureForSections columns
       await queryInterface.addColumn('LectureForSections', 'closeAttendanceAt', {
