5091151458c0ccb24cb94f569cc9272b8c327f0e
Nicholas Tanaka <118635840+MigrainePanda@users.noreply.github.com>
2025-12-07 23:33:08 -0800

    Update CORS implementation (#377)
    * Update CORS config for socket server
    
    * Add more methods to CORS options
    
    * Remove TODO CORS comment

--- socket/index.js
+++ socket/index.js
@@ -18,8 +18,10 @@ const server = http.createServer();
 const io = new Server(server, {
   cors: {
     origin: [process.env.CLIENT_URL || "http://localhost:3000"],
-    methods: ["GET", "POST"],
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    optionsSuccessStatus: 200,
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   },
 });
 
