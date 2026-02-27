ff34212b046590b5c50c930bc9d13054f8480b01
MigrainePanda <nicktanaka36@gmail.com>
2026-02-27 15:49:10 -0800

    Add empty settings page to fix navigation fail

---
+++ client/src/pages/Settings.jsx
@@ -0,0 +1,11 @@
+function Settings() {
+  return (
+    <>
+      <div className="singleCourseContainer">
+        <p>Settings have not been set up.</p>
+      </div>
+    </>
+  );
+}
+
+export default Settings;
