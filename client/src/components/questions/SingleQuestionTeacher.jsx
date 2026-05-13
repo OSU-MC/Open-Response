3f37a89f894749154ddc36cb07405aa0d7164575
MigrainePanda <nicktanaka36@gmail.com>
2026-05-12 23:21:49 -0700

    Fix navigation on question adding from lecture template

--- client/src/components/questions/SingleQuestionTeacher.jsx
+++ client/src/components/questions/SingleQuestionTeacher.jsx
@@ -1,11 +1,12 @@
-import { React, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { TailSpin } from "react-loader-spinner";
 import Notice from "../Notice";
 import apiUtil from "../../utils/apiUtil";
 import { useDispatch } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { addQuestion, stageQuestionInLecture } from "../../redux/actions";
 import useLectureQuestions from "@/hooks/useLectureQuestions";
+import { Button } from "react-bootstrap";
 
 function SingleQuestionTeacher(props) {
   const dispatch = useDispatch();
@@ -134,6 +135,7 @@ function SingleQuestionTeacher(props) {
           dispatch(stageQuestionInLecture(lectureId, response.data.question));
         }
         refreshLectureQuestions();
+        navigate(-1);
       }
     }
   };
@@ -246,6 +248,11 @@ function SingleQuestionTeacher(props) {
   if (editing) {
     return (
       <div className="vertical-container">
+        <Link className="" to={`/${courseId}/lectures/${lectureId}`}>
+          <Button className="back-btn">
+            <div id="back-btn-image" />
+          </Button>
+        </Link>
         {message != "" && error && (
           <Notice status={"error"} message={message} />
         )}
@@ -381,6 +388,11 @@ function SingleQuestionTeacher(props) {
   } else {
     return (
       <div className="vertical-container">
+        <Link className="" to={`/${courseId}/lectures/${lectureId}`}>
+          <Button className="back-btn">
+            <div id="back-btn-image" />
+          </Button>
+        </Link>
         <h1 className="question-stem">{question.stem}</h1>
         <form className="student-question-response-form">
           {Object.keys(options).map((index) => {
